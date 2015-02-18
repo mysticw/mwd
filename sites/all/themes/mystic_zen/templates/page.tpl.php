5fbf718a0c179ae357778b719352b4298f96b165
mysticw <john@mwdstudios.com>
2015-02-17 23:22:37 -0800

    fixed column layout of footer, added gray bkgd to second column footer and padding. Added padding-top to footer-wrapper and footer column blocks.

--- sites/all/themes/mystic_zen/templates/page.tpl.php
+++ sites/all/themes/mystic_zen/templates/page.tpl.php
@@ -57,42 +57,41 @@
 
   <div id="main">
 
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+    <?php print render($page['highlighted']); ?>
+    <div class="fixed-width">
+      <div id="content" class="column" role="main">
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $breadcrumb; ?>      
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+        
+      <?php
+        // Render the sidebars to see if there's anything in them.
+        $sidebar_first  = render($page['sidebar_first']);
+        $sidebar_second = render($page['sidebar_second']);
+      ?>  
+
+      <?php if ($sidebar_first || $sidebar_second): ?>
+        <aside class="sidebars">
+          <?php print $sidebar_first; ?>
+          <?php print $sidebar_second; ?>
+        </aside>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $breadcrumb; ?>      
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-      
-    <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-    ?>
-
-    <?php if ($sidebar_first || $sidebar_second): ?>
-      <aside class="sidebars">
-        <?php print $sidebar_first; ?>
-        <?php print $sidebar_second; ?>
-      </aside>
-    <?php endif; ?>
-      
-    </div>
-
-
-
-  </div>
+        
+      </div>
+    </div> <!-- fixed width -->
+  </div> <!-- main -->
 
   <?php if ($page['triptych_first'] || $page['triptych_second'] || $page['triptych_third']): ?>
     <div id="triptych-wrapper"><div id="triptych" class="clearfix">
